9c6ce50291c7cb42d040c6adfb77d3c727b7d52a
thinkvantagedu <duxiaohan2006@gmail.com>
2024-01-02 11:34:21 +0000

    Add ItemModel class

---
+++ Models/ItemModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace RankingApp.Models
+{
+	public class ItemModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public int ImageId { get; set; }
+		public int Ranking { get; set; }
+		public int ItemType { get; set; }
+		public ItemModel()
+		{
+		}
+	}
+}
+
